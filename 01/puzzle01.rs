09f3e23e7eeb88332671b06ce1cddf69044eba8e
Erik Weitenberg <eweitenberg@gmail.com>
2018-01-05 16:20:54 +0100

    it works!

--- 01/puzzle01.rs
+++ 01/puzzle01.rs
@@ -10,20 +10,19 @@ fn main() {
 		.lines() // no newline bytes; is an iterator of Result<String> (which is String or Err)
 		.next() // only the first line is relevant; is an Option<_> (which is Result<_> or None)
 		.unwrap() // returns the Result<_> in Option<_>, or panics if there is None
-		.unwrap(); // returns the string in Result<String>, or panics if it's an error
+		.unwrap(); // returns the String in Result<String>, or panics if it's an error
 
-	let (x, y) : (i32, i32) = // a tuple of integers
-		input.split(", ") // split it at the given pattern
+	let directions : Vec<(i32, i32)> = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
+
+	let (_, x, y) : (usize, i32, i32) = // a tuple of integers
+		input.split(", ") // split it at the given pattern; is an iterator of Strings
 		.map(|s| s.split_at(1)) // gives us (&str, &str) like ("L", "12")
-		.map(|(s, n)| (s, n.parse::<i32>().unwrap())) // it becomes ("L", 12)
-		.map(|pair| match pair {
-			("L", n) => (-n, 0),
-			("R", n) => (n, 0),
-			("U", n) => (0, -n),
-			("D", n) => (0, n),
-			_ => panic!(pair)
-		}) // turns them into relative position coordinates like (-12, 0)
-		.fold((0, 0), |(x, y), (dx, dy)| (x + dx, y + dy)); // and adds them up
+		.map(|(s, n)| (match s {"L" => 3, "R" => 1,	_ => panic!(format!("{:?}", s))},
+		               n.parse::<i32>().unwrap())) // it becomes (3, 12)
+		.fold((0, 0, 0), |(d, x, y), (dir, dist)| { 
+			let nd = (d + dir) % 4; 
+			(nd, x + dist * directions[nd].0, y + dist * directions[nd].1)
+		}); // and adds them up
 			// This last line is a consumer, and it is the only reason the otherwise lazy
 			// iterators will actually do anything
 
