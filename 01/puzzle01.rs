f4752d454299e3a13eda59a23b750ef366c827c7
Erik Weitenberg <eweitenberg@gmail.com>
2018-01-05 16:51:31 +0100

    Solved day 1 part 2

--- 01/puzzle01.rs
+++ 01/puzzle01.rs
@@ -1,5 +1,6 @@
 use std::io; // provides io's stdin()
 use std::io::BufRead; // provides lines()
+use std::collections::HashSet;
 
 fn main() {
 	// Get the first line from standard input
@@ -11,6 +12,8 @@ fn main() {
 		.next() // only the first line is relevant; is an Option<_> (which is Result<_> or None)
 		.unwrap() // returns the Result<_> in Option<_>, or panics if there is None
 		.unwrap(); // returns the String in Result<String>, or panics if it's an error
+
+	// Part A. Find out how far you are from the starting point
 
 	let directions : Vec<(i32, i32)> = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
 
@@ -26,5 +29,29 @@ fn main() {
 			// This last line is a consumer, and it is the only reason the otherwise lazy
 			// iterators will actually do anything
 
-	println!("{}", x.abs() + y.abs())
+	println!("{}", x.abs() + y.abs());
+
+	// Part B. Stop at the first revisited node.
+
+	let instructions : Vec<(usize, i32)> = input.split(", ").map(|s| s.split_at(1))
+		.map(|(s, n)| (match s {"L" => 3, "R" => 1,	_ => panic!(format!("{:?}", s))},
+		               n.parse::<i32>().unwrap())) // it becomes (3, 12)
+		.collect(); // from iterator to vector
+
+	let mut visited = HashSet::new();
+	let (mut dir, mut x, mut y) = (0usize, 0i32, 0i32);
+	visited.insert((x,y));
+
+'search:
+	for instruction in instructions {
+		dir = (dir + instruction.0) % 4;
+		for _ in 0..instruction.1 {
+			x += directions[dir].0;
+			y += directions[dir].1;
+			if !visited.insert((x,y)) {
+				println!("{}", x.abs() + y.abs());
+				break 'search
+			}
+		}
+	}
 }
